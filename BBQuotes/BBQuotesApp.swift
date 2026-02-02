3813c76d139716eb0459cf23bb785a796e50fbd6
Ritika <bhardwajritika11@gmail.com>
2026-02-03 00:15:06 +0530

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Tarun Sharma on 03/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
